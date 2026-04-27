b3227727b4214fdfdbc1700c666807847c4d8880
SudipDas1808011 <sudipdas11804144@gmail.com>
2026-04-27 12:18:51 +0600

    feat: add get api to fetch data

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,6 +1,21 @@
 import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 
+export async function GET() {
+  try {
+    const client = await clientPromise;
+    const db = client.db("taskdb");
+
+    const tasks = await db.collection("tasks").find().toArray();
+
+    return NextResponse.json(tasks);
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ success: false, error: "Failed to fetch tasks" }, { status: 500 });
+  }
+}
+
+
 export async function POST(request: Request) {
   try {
     const client = await clientPromise;
